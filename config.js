72d90554f534ebef6159daaedc41649ec1dd8bfa
JPDeveloper1 <josefernandovip39@gmail.com>
2024-11-06 21:59:38 -0400

    test

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const DATABASE_URL = process.env.DATABASE_URL || "http://localhost:3001"
+export const DATABASE_URL = process.env.DATABASE_URL || "postgresql://smart_brain_om63_user:sG7mBzFnosevSw5Ovq72ydN5v4hbH2T0@dpg-cshjk0aj1k6c73993bb0-a/smart_brain_om63"
 
 export const DB_HOST = process.env.DB_HOST  || 'localhost'
 export const PORT= process.env.PORT || 3000
