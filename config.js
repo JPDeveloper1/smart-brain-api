10416c3135d8e81d6cc636001cfc732a7aa6060a
JPDeveloper1 <josefernandovip39@gmail.com>
2024-11-06 22:13:08 -0400

    fixing conectividad

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const DATABASE_URL = process.env.DATABASE_URL || "http://localhost:3000/"
 
-export const DB_HOST = process.env.DB_HOST  || 'localhost'
-export const PORT= process.env.PORT || 3000
+export const DB_HOST = process.env.DB_HOST  || '127.0.0.1'
+export const PORT= process.env.PORT || 5432
 export const DB_DATABASE= process.env.DB_DATABASE || "smart-brain"
 export const DB_USER= process.env.DB_USER ||"postgres"
 export const  DB_PASSWORD=process.env.DB_PASSWORD || "guzman80944"
